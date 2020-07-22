adb71c3947631d4e8b2bf33d1ef9a25e257ed1b3
AleksanderSkubala <alexskubala@gmail.com>
2020-07-22 15:55:27 +0200

    Clear makapaka

--- README.md
+++
@@ -1,5 +0,0 @@
-Hello!
-
-You discovered me, congrats!
-
-Don't worry I'll change in the future...
--- gatsby-node.js
+++
@@ -1,7 +0,0 @@
-exports.createPages = ({ actions }) => {
-  const { createRedirect } = actions
-  createRedirect({ fromPath: '/facebook', toPath: 'https://facebook.com', redirectInBrowser: true, isPermanent: true })
-  createRedirect({ fromPath: '/twitter', toPath: 'https://twitter.com/triplea_dev', redirectInBrowser: true, isPermanent: true })
-  createRedirect({ fromPath: '/discord', toPath: 'https://discord.gg/xFSWRM5', redirectInBrowser: true, isPermanent: true })
-  createRedirect({ fromPath: '/instagram', toPath: 'https://instagram.com', redirectInBrowser: true, isPermanent: true })
-}
