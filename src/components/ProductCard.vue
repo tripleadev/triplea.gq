e6a7ffc2be3bd91043773d150424b237af393a8a
datejer <ejer.priv@wp.pl>
2020-09-13 09:59:30 +0200

    Add hover shadow effect to product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,46 +1,51 @@
 <template>
-  <div class="cardWrapper">
-    <h3>{{name}}</h3>
-    <p>{{description}}</p>
-  </div>
+	<div class="cardWrapper">
+		<h3>{{ name }}</h3>
+		<p>{{ description }}</p>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'ProductCard',
-  props: [ 'name', 'description' ]
-}
+	name: "ProductCard",
+	props: ["name", "description"],
+};
 </script>
 
 <style lang="scss">
- .cardWrapper {
-   color: unset;
-   font-weight: normal;
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   padding: 30px;
-   background-color: white;
-   width: 70vw;
-   height: 45vh;
-   border-radius: 7px;
-   box-shadow: 0px 0px 20px -5px rgba(0,0,0,0.3);
-   margin: 15px;
+.cardWrapper {
+	color: unset;
+	font-weight: normal;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	padding: 30px;
+	background-color: white;
+	width: 70vw;
+	height: 45vh;
+	border-radius: 7px;
+	box-shadow: 0px 0px 20px -5px rgba(0, 0, 0, 0.3);
+	margin: 15px;
+	transition: box-shadow 0.3s ease;
 
-   h3 {
-     color: black;
-   }
+	h3 {
+		color: black;
+	}
 
-   p {
-     text-align: justify;
-   }
- }
+	p {
+		text-align: justify;
+	}
+}
 
-  @media (min-width: 768px) {
-    .cardWrapper {
-      width: 50vw;
-      height: unset;
-      margin: 0 0 15px 0;
-    }
-  }
+@media (min-width: 768px) {
+	.cardWrapper {
+		width: 50vw;
+		height: unset;
+		margin: 0 0 15px 0;
+	}
+}
+
+.cardWrapper:hover {
+	box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.3);
+}
 </style>
